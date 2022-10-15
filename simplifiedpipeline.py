fe9d56556fb4fbaa79c9164031055c492d0f455b
Andrea Casagranda <ybblkblt@yahoo.com>
2022-10-15 18:03:15 -0400

    working on logic

--- simplifiedpipeline.py
+++ simplifiedpipeline.py
@@ -3,15 +3,15 @@
 
 
 
-caches = [-1,-1,-1,-1,-1]
-memory = {100:0,101:0,102:0}
-instructions = ['LI $1 4','LI $2 5','LI $3 6', 'MUL $1 $2 $4','MUL $3 $4 $4', 'MUL $1 $3 $1', 'MUL $2 $3 $4','ADD $1 $4 $2', 'LW $3 $104', 'ADD $2 $3 $4', 'ADD $4 $4 $1']
+caches = {}
+instructions = ['LI $1 4','LI $2 5','LI $3 6', 'MUL $1 $2 $4','SW $4 $100','MUL $3 $4 $4', 'MUL $1 $3 $1', 'MUL $2 $3 $2','ADD $1 $2 $1', 'LW $3 $100', 'ADD $1 $3 $2', 'ADD $2 $2 $3']
 instr_len = len(instructions)
 instr_var = {}
 stall = {}
-for idx in range(len(instructions)):
+for idx in range(len(instructions)+5):
     stall[idx]=0
 stall[-1] = 0
+stall[-2] = 0
 instr_c = 0
 decodes_c = -1
 executes_c = -2
@@ -24,10 +24,16 @@ class mul:
         self.instr_list = instr_list
         self.sources = [self.instr_list[1],self.instr_list[2]]
         self.destination = self.instr_list[3]
+        self.executes = ''
+        self.result = 0
+        self.mem_access = ''
         self.decodes = f"Multiply value in {self.sources[0]} by value in {self.sources[1]} and put result into {self.destination}. "
-        self.result = caches[int(self.sources[0][1])]*caches[int(self.sources[1][1])]
-        self.executes = f"{caches[int(self.sources[0][1])]} x {caches[int(self.sources[1][1])]} = {self.result}"
+        
+    def calc_executes(self):
+        self.result = int(caches[self.sources[0]]*caches[self.sources[1]])
+        self.executes = f"{caches[self.sources[0]]} x {caches[self.sources[1]]} = {self.result}"
         self.mem_access = f"{self.result} -> {self.destination} "
+        
 
 class add:
     def __init__(self,instr_list,instr_c):
@@ -35,9 +41,14 @@ class add:
         self.instr_list = instr_list
         self.sources = [self.instr_list[1],self.instr_list[2]]
         self.destination = self.instr_list[3]
+        self.executes = ''
+        self.result = 0
+        self.mem_access = ''
         self.decodes = f"Add value in {self.sources[0]} to value in {self.sources[1]} and put result into {self.destination}. "
-        self.result = caches[int(self.sources[0][1])]+caches[int(self.sources[1][1])]
-        self.executes = f"{caches[int(self.sources[0][1])]} + {caches[int(self.sources[1][1])]} = {self.result}"
+        
+    def calc_executes(self):
+        self.result = int(caches[self.sources[0]]+caches[self.sources[1]])
+        self.executes = f"{caches[self.sources[0]]} + {caches[self.sources[1]]} = {self.result}"
         self.mem_access = f"{self.result} -> {self.destination} "
 
 class li:
@@ -46,21 +57,27 @@ class li:
         self.instr_list = instr_list
         self.sources = []
         self.destination = self.instr_list[1]
-        self.result = self.instr_list[2]
+        self.result = int(self.instr_list[2])
         self.decodes = f"Put value {self.result} into {self.destination}"
         self.executes = "no calculation"
         self.mem_access = f"{self.result} -> {self.destination} "
+    
+    def calc_executes(self):
+        return
 
 class sw:
     def __init__(self,instr_list,instr_c):
         self.instr_c = instr_c
         self.instr_list = instr_list
-        self.sources = []
+        self.sources = [self.instr_list[1]]
         self.destination = self.instr_list[2]
-        self.result = self.instr_list[1]
-        self.decodes = f"Take value from {self.result} and put into memory {self.destination}"
+        self.result = 0
+        self.decodes = f"Take value from {self.instr_list[1]} and put into memory {self.destination}"
         self.executes = "no calculation"
-        self.mem_access = f"NULL -> {self.result} "
+        self.mem_access = "no change in cache"
+
+    def calc_executes(self):
+        self.result = int(caches[self.instr_list[1]])
 
 class lw:
     def __init__(self,instr_list,instr_c):
@@ -68,9 +85,13 @@ class lw:
         self.instr_list = instr_list
         self.sources = []
         self.destination = self.instr_list[1]
-        self.result = self.instr_list[2]
-        self.decodes = f"Take value from memory {self.result} and put into {self.destination}"
+        self.result = 0
+        self.decodes = f"Take value from memory {self.instr_list[2]} and put into {self.destination}"
         self.executes = "no calculation"
+        self.mem_access = " "
+
+    def calc_executes(self):
+        self.result = int(caches[self.instr_list[2]])
         self.mem_access = f"{self.result} -> {self.destination} "
 
 for indx in range(instr_len):
@@ -89,51 +110,73 @@ for indx in range(instr_len):
         case _:
             print(f"Invalid instruction: {instructions[indx]} ")
             instr_c +=10
-for indx in range(1,instr_len-1):
+for indx in range(1,instr_len):
     if instr_var[indx-1].destination in instr_var[indx].sources:
-        stall[idx]= 1
-
+        stall[indx]= 1
+      
+    
+      
+stall1 = False
+stall2 = False
 #print intro here
 
 
 print("-------------------------------------------")
 
 
-while instr_c < instr_len + 4:
+while mem_access_c < instr_len:
     input("Press ENTER to display the next cycle:")
-    if instr_c < instr_len and stall[instr_c]!=1:
+    if instr_c < instr_len and not stall1:
         print(f"fetch instruction {instr_c}: {instructions[instr_c]}")
+        instr_c += 1
     elif instr_c >= instr_len:
         print("fetch instruction: no more instructions to fetch")
     else:
         print("fetch instruction: stalling ...")
-    if decodes_c > -1 and instr_c < instr_len + 1 and stall[instr_c]!=1:
+
+    if decodes_c > -1 and decodes_c < instr_len and not stall1:
         print(f"decode instruction {decodes_c}: {instr_var[decodes_c].decodes}")
         decodes_c +=1
-    elif instr_c >= instr_len + 1:
+    elif decodes_c >= instr_len:
         print("decode instruction: no more instructions to decode")
     elif decodes_c < 0:
         print("decode instruction: no instructions to decode yet")
+        decodes_c +=1
     else:
         print("decode instruction: stalling ...")
-    if executes_c > -1 and instr_c < instr_len + 2 and stall[instr_c]!=1:
+    
+
+    if executes_c > -1 and executes_c < instr_len and not stall1: 
+        instr_var[executes_c].calc_executes()
         print(f"execute instruction {executes_c}: {instr_var[executes_c].executes}")
         executes_c +=1
-    elif instr_c >= instr_len + 2:
+    elif executes_c >= instr_len:
         print("execute instruction: no more instructions to execute")
     elif executes_c < 0:
         print("execute instruction: no instructions to execute yet")
+        executes_c +=1
     else:
         print("execute instruction: stalling ...")
-
-    if mem_access_c > -1 and stall[instr_c]!=1:
+        #instr_var[executes_c].calc_executes()
+    
+    if mem_access_c > -1 and mem_access_c < instr_len and not stall2:
         print(f"memory access instruction {mem_access_c}: {instr_var[mem_access_c].mem_access}")
+        caches[instr_var[mem_access_c].destination] = int(instr_var[mem_access_c].result)
+        mem_access_c +=1
+    elif mem_access_c >= instr_len:
+        print("memory access instruction: no more instructions")
+    elif mem_access_c < 0:
+        print("memory access instruction: no memory access yet")
         mem_access_c +=1
     else:
-        print("memory access instruction: no memory to access yet")
-    if stall[instr_c]==1:
-        stall[instr_c]==0
-    else:
-        instr_c += 1
+        print("memory access instruction: no change in cache2")
+
+    if stall[instr_c-2]==1:
+        stall1 = not stall1
+    if stall[mem_access_c]==1:
+        stall2 = not stall2
+
 
     print("-------------------------------------------")
+
+print(caches['$3'],caches['$4'])
